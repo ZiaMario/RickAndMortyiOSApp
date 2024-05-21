56d04f5e45bc37908c125a4076b35aeec5891ca0
jessicaolivera <31460170+jessicaolivera@users.noreply.github.com>
2024-05-21 13:52:36 -0600

    Initial Commit

---
+++ RickandMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMorty
+//
+//  Created by JESSICA OLIVERA on 21/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
